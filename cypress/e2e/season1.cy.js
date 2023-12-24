e8af199e2ecad93e571a101e9b793b1b16299328
Nasser Ramezanpour <naser.ramezanpour@gmail.com>
2023-12-24 20:11:11 +0330

    ad suit

--- cypress/e2e/season1.cy.js
+++ cypress/e2e/season1.cy.js
@@ -1,6 +1,25 @@
 /// <reference types ="cypress" />
 
+// it('Google Search', function () {
+//     cy.visit("https://google.com");
+//     cy.get("input[name=q]").type("Cypress{enter}")
+// });
+//
+// it('2- Cypress Website', function () {
+//     cy.visit("https://cypress.io");
+//     cy.title().should('eq', 'JavaScript Component Testing and E2E Testing Framework | Cypress')
+//     cy.title().should('include', 'E2E Testing Framework | Cypress')
+// });
+
+describe('Suit 1', () => {
 it('Google Search', function () {
     cy.visit("https://google.com");
     cy.get("input[name=q]").type("Cypress{enter}")
 });
+
+it('2- Cypress Website', function () {
+    cy.visit("https://cypress.io");
+    cy.title().should('eq', 'JavaScript Component Testing and E2E Testing Framework | Cypress')
+    cy.title().should('include', 'E2E Testing Framework | Cypress')
+});
+})
