fda8f5dbf67b9b7efec018089d3e0b3c763d9693
nasser ramezanpour <na.rmpr@gmail.com>
2024-01-14 09:21:11 +0330

    add test form alert in session 6

--- cypress/e2e/season6_spec.cy.js
+++ cypress/e2e/season6_spec.cy.js
@@ -18,7 +18,7 @@ describe('session 6', () => {
         cy.get("#result").invoke('text').should('equal', 'You clicked: Ok');
     })
 
-    it.only("2-2 Alerts - Confirm - Dismiss", () => {
+    it("2-2 Alerts - Confirm - Dismiss", () => {
         cy.visit("https://the-internet.herokuapp.com/javascript_alerts")
         cy.contains("Click for JS Confirm").click();
         cy.on("window:confirm", function(message) {
@@ -30,6 +30,25 @@ describe('session 6', () => {
     })
 
     it("3- Alerts - Prompt", () => {
+        cy.visit("https://the-internet.herokuapp.com/javascript_alerts")
+        cy.window().then(function($win){
+            cy.contains("Click for JS Prompt").click();
+            cy.stub($win, 'prompt').returns('AutomationCamp-cypress');
+        })
+        cy.get("#result").should('have.text', 'You entered: AutomationCamp-cypress');
+        cy.get("#result").invoke('text').should('equal', 'You entered: AutomationCamp-cypress');
+    })
 
+    it.only("4- Alerts - Prompt", () => {
+        cy.visit("https://material.angular.io/components/dialog/examples");
+        cy.get('body > material-docs-app > app-cookie-popup > div > div > button > span.mdc-button__label');
+        cy.get('body > material-docs-app > app-navbar > nav.docs-navbar-header > a:nth-child(3)').then(function(cdk) {
+            cy.get('#dialog-data > div > div.docs-example-viewer-body.ng-star-inserted > dialog-data-example > button').click();
+            cy.wait(1000);
+            let rect = cdk[0].getBoundingClientRect()
+            cy.document().then(function(doc) {
+                doc.elementFromPoint(rect.x, rect.y).click();
+            })
+        })
     })
 })
